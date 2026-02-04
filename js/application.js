7e4fef9a104141a60eaa88df69e269dc1abdcd54
Stanley Ho <stanleyho862@gmail.com>
2026-02-03 23:00:20 -0800

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,7 +1,7 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-  var leaderboard = new LeaderboardManager('https://script.google.com/macros/s/AKfycbw5MCKtkyy5iWIKQbx6AyYy7Gno4HTZjOraOmDVW00lD7AkkAcTSGdfk5F4bRuNndgJ/exec');
+  var leaderboard = new LeaderboardManager('https://script.google.com/macros/s/AKfycbwbWGvDVof2KlUkNwOPfqDLvb8uTsgo7UsLlhmGl8jQ7F3G8lqUrnWDxPG4yf90i0O1/exec');
   window.gameLeaderboard = leaderboard;
   leaderboard.createPermanentLeaderboard();
 
